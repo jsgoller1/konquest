2fc3e2bc892340d14991f9426e7db363bcf330cc
BigJon00 <anime4dahomies@gmail.com>
2025-11-12 09:01:45 -0500

    The Behavior states / FSM so far
    WIP. Please check and lmk what I should change if I need to
    I have the states:
    Searching - Default; go in random direction
    Chasing - Found a player unit; go after them
    Fighting - Next to a player unit; fight
    Fleeing - Low health; run away
    Dead - No health or off screen

--- src/Main.java
+++ src/Main.java
@@ -26,11 +26,18 @@ class Main {
             GAME_RUNNING = false;
         }
 
+        Enemy dummyEnemy = new Enemy("Dummy", 100, 10, 5); // to delete
+        // Add a dummy enemy here; not actually on game board,
+            // so won't show up on screent
         while (GAME_RUNNING) {
             long time = System.currentTimeMillis();
             inputManager.update(board);
             window.update(board, time);
 
+            // To test your FSM, try mutating the state of the enemy directly 
+            // here, and then print what state they're in after.
+            dummyEnemy.setHp = 0; // Example: set health to 0 to test "dead" state
+            // Print the enemy's current state, should be "dead"
         }
     }
 }
